e9fc8433f7740224b8cb2bf67ad4ae2d2c89014f
nibblenibble123 <93320829+nibblenibble123@users.noreply.github.com>
2021-10-28 18:07:42 +0100

    Update faces.py

--- faces.py
+++ faces.py
@@ -4,7 +4,7 @@ from time import sleep
 import random
 from gpiozero import Button
 
-faceValue = 0
+faceValue = 1
 
 faceButton = Button(2)
 textButton = Button(7)
@@ -22,10 +22,58 @@ sleep(2)
 
 while True:
   faceButton.wait_for_press()
+  faceValue + 1
+  if faceValue == 4:
+     faceValue = 1
   
+  if faceValue = 1:
+    #smile
+    pixels.show()
+    pixels.fill((0, 0, 0))
+    
+    pixels[14] = (0, 255, 0)
+    pixels[2] = (0, 255, 0)
+    pixels[3] = (0, 255, 0)
+    pixels[4] = (0, 255, 0)
+    pixels[5] = (0, 255, 0)
+    pixels[9] = (0, 255, 0)
+    pixels[34] = (0, 255, 0)
+    pixels[29] = (0, 255, 0)
+    pixels[26] = (0, 255, 0)
+    pixels[37] = (0, 255, 0)
+  else faceValue = 2:
+    #no emotion
+    pixels.show()
+    pixels.fill((0, 0, 0))
+    
+    pixels[14] = (0, 255, 0)
+    pixels[13] = (0, 255, 0)
+    pixels[12] = (0, 255, 0)
+    pixels[11] = (0, 255, 0)
+    pixels[10] = (0, 255, 0)
+    pixels[9] = (0, 255, 0)
+    pixels[34] = (0, 255, 0)
+    pixels[29] = (0, 255, 0)
+    pixels[26] = (0, 255, 0)
+    pixels[37] = (0, 255, 0)
+   elif faceValue = 3:
+    #frown 
+    pixels.show()
+    pixels.fill((0, 0, 0))
+    
+    pixels[6] = (0, 255, 0)
+    pixels[1] = (0, 255, 0)
+    pixels[13] = (0, 255, 0)
+    pixels[12] = (0, 255, 0)
+    pixels[11] = (0, 255, 0)
+    pixels[10] = (0, 255, 0)
+    pixels[34] = (0, 255, 0)
+    pixels[29] = (0, 255, 0)
+    pixels[26] = (0, 255, 0)
+    pixels[37] = (0, 255, 0)
   
-  int joe = random.randrange(1, 300)
-  if joe == 30
+  joe = random.randrange(1, 300)
+  if joe == 30:
     pixels[34] = (0, 0, 0)
     pixels[30] = (0, 255, 0)
     sleep(2)
