13cea2d4dea463eff83f3793fa5e1db14ff98f76
nibblenibble123 <93320829+nibblenibble123@users.noreply.github.com>
2021-10-31 15:06:02 +0000

    Update YO_MAMA.py

--- YO_MAMA.py
+++ YO_MAMA.py
@@ -13,7 +13,40 @@ pixels[31] = (0, 255, 0)
 pixels[16] = (0, 255, 0)
 
 sleep (0.1)
-
 pixels.fill((0, 0, 0))
 
-pixels[
+pixels[33] = (0, 255, 0)
+pixels[30] = (0, 255, 0)
+pixels[17] = (0, 255, 0)
+pixels[0] = (0, 255, 0)]
+pixels[16] = (0, 255, 0)
+pixels[15] = (0, 255, 0)
+
+sleep (0.1)
+pixels.fill((0, 0, 0))
+
+pixels[32] = (0, 255, 0)
+pixels[31] = (0, 255, 0)
+pixels[16] = (0, 255, 0)
+pixels[17] = (0, 255, 0)
+pixels[14] = (0, 255, 0)
+pixels[1] = (0, 255, 0)
+pixels[34] = (0, 255, 0)
+pixels[29] = (0, 255, 0)
+pixels[18] = (0, 255, 0)
+
+sleep (0.1)
+pixels.fill((0, 0, 0))
+
+pixels[35] = (0, 255, 0)
+pixels[28] = (0, 255, 0)
+pixels[19] = (0, 255, 0)
+pixels[18] = (0, 255, 0)
+pixels[13] = (0, 255, 0)
+pixels[2] = (0, 255, 0)
+pixels[33] = (0, 255, 0)
+pixels[30] = (0, 255, 0)
+pixels[17] = (0, 255, 0)
+
+sleep (0.1)
+pixels.fill((0, 0, 0))
